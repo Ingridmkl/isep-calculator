b6aebb77a319772284c541a6939e3fde03cfd74a
Ingridmkl <ingrid.moukala@eleve.isep.fr>
2025-05-06 23:49:00 +0200

    Modify evaluateMathExpression method to handle varied expressions with additions multiplications and substractions

--- src/main/java/org/isep/cleancode/calculator/Calculator.java
+++ src/main/java/org/isep/cleancode/calculator/Calculator.java
@@ -1,23 +1,42 @@
 package org.isep.cleancode.calculator;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Calculator {
     public double evaluateMathExpression(String expression) {
         // \\s+ -> reg ex to match any whitespace
-        String noSpacesExpression = expression.replaceAll("\\s+", "");
+        List<String> expressionArray = expressionToArray(expression.replaceAll("\\s+", ""));
 
-        if (noSpacesExpression.contains("+")) {
-            return calculate(noSpacesExpression, "\\+");
-        } else if (noSpacesExpression.contains("-")) {
-            return calculate(noSpacesExpression, "\\-");
-        } else if (noSpacesExpression.contains("*")) {
-            return calculate(noSpacesExpression, "\\*");
-        } else {
-            return Double.parseDouble(noSpacesExpression);
+        for (int i = 0; i < expressionArray.size(); i++) {
+            if (expressionArray.get(i).equals("*")) {
+                double leftDigit = Double.parseDouble((expressionArray.get(i-1)));
+                double rightDigit = Double.parseDouble((expressionArray.get(i+1)));
+                double result = leftDigit * rightDigit;
+
+                expressionArray.set(i-1, Double.toString(result));
+                expressionArray.subList(i, i + 2).clear();
+
+                i--;
+            }
         }
+
+        double res = Double.parseDouble(expressionArray.getFirst());
+        for (int i = 1; i < expressionArray.size(); i+=2) {
+            String operator = expressionArray.get(i);
+            double nextNumber = Double.parseDouble(expressionArray.get(i + 1));
+
+            if (operator.equals("+")) {
+                res += nextNumber;
+            } else if (operator.equals("-")) {
+                res -= nextNumber;
+            }
+        }
+
+        return res;
     }
 
+    /*
     private double calculate(String expression, String operator) {
         // This method contains all the calculation logic
         String[] stringSplit = expression.split(operator);
@@ -35,5 +54,28 @@ public class Calculator {
         }
 
         return res;
+    }
+    */
+
+    private List<String> expressionToArray(String expression) {
+        List<String> stringList = new ArrayList<>();
+        StringBuilder number = new StringBuilder();
+
+        for (char character : expression.toCharArray() ) {
+            if (Character.isDigit(character) || character == '.') {
+                number.append(character);
+            } else {
+                if (!number.isEmpty()) {
+                    stringList.add(number.toString());
+                    number.setLength(0);
+                }
+                stringList.add(String.valueOf(character));
+            }
+        }
+        if (!number.isEmpty()) {
+            stringList.add(number.toString());
+        }
+
+        return stringList;
     }
 }
