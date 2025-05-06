6a95a9c99dd6a24b615abcd40f1ec4da6b75ba83
Ingridmkl <ingrid.moukala@eleve.isep.fr>
2025-05-06 21:34:02 +0200

    Modify Calculator class to refactor and implement methods

--- src/main/java/org/isep/cleancode/calculator/Calculator.java
+++ src/main/java/org/isep/cleancode/calculator/Calculator.java
@@ -4,119 +4,36 @@ import java.util.Arrays;
 
 public class Calculator {
     public double evaluateMathExpression(String expression) {
-        //  basicPositiveInteger and basicFloatingPointNumber
-        // return Double.parseDouble(expression);
-
-        /* basicAddition
-        // \s+ -> reg ex to match any whitespace
+        // \\s+ -> reg ex to match any whitespace
         String noSpacesExpression = expression.replaceAll("\\s+", "");
 
         if (noSpacesExpression.contains("+")) {
-            // Split string by +
-            String[] stringSplit = noSpacesExpression.split("\\+");
-
-            double num1 = Double.parseDouble(stringSplit[0]);
-            double num2 = Double.parseDouble(stringSplit[1]);
-
-            return num1 + num2;
-        } else {
-            return Double.parseDouble(noSpacesExpression);
-        }
-         */
-
-        /* multiple additions
-
-        // \s+ -> reg ex to match any whitespace
-        String noSpacesExpression = expression.replaceAll("\\s+", "");
-
-        if (noSpacesExpression.contains("+")) {
-            // Split string by +
-            String[] stringSplit = noSpacesExpression.split("\\+");
-
-            double sum = 0;
-
-            for (String num : stringSplit) {
-                sum += Double.parseDouble(num);
-            }
-
-            return sum;
-        } else {
-            return Double.parseDouble(noSpacesExpression);
-        }
-         */
-
-        /* subtractions
-
-        // \s+ -> reg ex to match any whitespace
-        String noSpacesExpression = expression.replaceAll("\\s+", "");
-
-        if (noSpacesExpression.contains("+")) {
-            // Split string by '+'
-            String[] stringSplit = noSpacesExpression.split("\\+");
-
-            double sum = 0;
-
-            for (String num : stringSplit) {
-                sum += Double.parseDouble(num);
-            }
-
-            return sum;
+            return calculate(noSpacesExpression, "\\+");
         } else if (noSpacesExpression.contains("-")) {
-            // Split string by '-'
-            String[] stringSplit = noSpacesExpression.split("\\-");
-
-            double sum = Double.parseDouble(stringSplit[0]);
-
-            for (int i = 1; i < stringSplit.length; i++) {
-                sum -= Double.parseDouble(stringSplit[i]);
-            }
-
-            return sum;
-        } else {
-            return Double.parseDouble(noSpacesExpression);
-        }
-         */
-
-        // multiplications
-
-        // \s+ -> reg ex to match any whitespace
-        String noSpacesExpression = expression.replaceAll("\\s+", "");
-
-        if (noSpacesExpression.contains("+")) {
-            // Split string by '+'
-            String[] stringSplit = noSpacesExpression.split("\\+");
-
-            double sum = 0;
-
-            for (String num : stringSplit) {
-                sum += Double.parseDouble(num);
-            }
-
-            return sum;
-        } else if (noSpacesExpression.contains("-")) {
-            // Split string by '-'
-            String[] stringSplit = noSpacesExpression.split("-");
-
-            double sum = Double.parseDouble(stringSplit[0]);
-
-            for (int i = 1; i < stringSplit.length; i++) {
-                sum -= Double.parseDouble(stringSplit[i]);
-            }
-
-            return sum;
+            return calculate(noSpacesExpression, "\\-");
         } else if (noSpacesExpression.contains("*")) {
-            // Split string by '*'
-            String[] stringSplit = noSpacesExpression.split("\\*");
-
-            double sum = Double.parseDouble(stringSplit[0]);
-
-            for (int i = 1; i < stringSplit.length; i++) {
-                sum *= Double.parseDouble(stringSplit[i]);
-            }
-
-            return sum;
+            return calculate(noSpacesExpression, "\\*");
         } else {
             return Double.parseDouble(noSpacesExpression);
         }
+    }
+
+    private double calculate(String expression, String operator) {
+        // This method contains all the calculation logic
+        String[] stringSplit = expression.split(operator);
+
+        double res = Double.parseDouble(stringSplit[0]);
+
+        for (int i = 1; i < stringSplit.length; i++) {
+            double num = Double.parseDouble(stringSplit[i]);
+
+            switch (operator) {
+                case "\\+" -> res += num;
+                case "\\-" -> res -= num;
+                case "\\*" -> res *= num;
+            }
+        }
+
+        return res;
     }
 }
